aaa5469469ed9a07a6ab9960d9c457ca2a964f46
Esteban Medina Palacio <estebanmedina9@gmail.com>
2025-02-06 23:40:52 -0500

    feat: Initial config for hashing password

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,18 @@
-import express from "express"
-import dotenv from "dotenv"
-import {connectDB} from "./lib/db.js"
-import authRoutes from "./routes/auth.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT
+app.use(express.json());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log("Server running on PORT:", PORT);
-  connectDB()
+  connectDB();
 });
-f
+f;
