02db82722be83d8b5acf36198f329cc02412e624
Esteban Medina Palacio <estebanmedina9@gmail.com>
2025-02-07 23:29:41 -0500

    feat: Update auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,32 @@
-export const signup = (req, res) => {
-  const { fullName, email, password } = req.body
-  try{
-    // hash password
-    
-  } catch (error) {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-  }
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser){
+      res.status(400).json({message:"Invalid user data"})
+    }
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
